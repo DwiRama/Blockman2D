05b109465c9af8de3d1babd7b96735b228780906
Decoir <dwiramamalawat10@gmail.com>
2018-10-27 14:51:39 +0700

    FIrst Commit

---
+++ Blockman 2D/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GameMaster : MonoBehaviour {
+
+    public static GameMaster _GM;
+
+    public PlayerMovement playerMovement;
+    public Text coinsCollectText;
+    public AudioSource music;
+    public bool victory = false;
+    public Image clearMessage;
+
+    public SceneConnector SceneConnector;
+    public string nextScene;
+
+    public UnityEvent OnVictory;
+
+    private int coinCounter;
+
+    void Awake()
+    {
+        _GM = this;
+        if (OnVictory == null)
+            OnVictory = new UnityEvent();
+    }
+
+	public void GetCoin(int poin)
+    {
+        coinCounter += poin;
+        UpdateCoinCounterText();
+    }
+
+    public void UpdateCoinCounterText()
+    {
+        coinsCollectText.text = "Coins: " + coinCounter;
+    }
+    
+    public void Victory(AudioSource victoryMusic)
+    {
+        playerMovement.ToggleMovement(false);
+        music.Stop();
+        victoryMusic.Play();
+        victory = true;
+        clearMessage.enabled = true;
+        OnVictory.Invoke();
+    }
+
+    public void LoadScene()
+    {
+        StartCoroutine(LoadingScene(5f));
+    }
+
+    IEnumerator LoadingScene(float waitTime = 0)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneConnector.LoadScene(nextScene);
+        yield return null;
+    }
+}
---
+++ Blockman 2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller; // Class that handles character physics
+    public Animator animator;
+    public AudioSource jumpSound;
+
+    public float runSpeed = 40; //Amount of speed to give to a player
+
+    float horizontalMove = 0f; //Movement Direction
+    bool jump = false;
+    bool cannotMove = false;
+
+    public void ToggleMovement(bool status)
+    {
+        cannotMove = !status;
+    }//
+
+	void Update() {
+        if (cannotMove)
+        {
+            animator.SetFloat("Speed", 0);
+            return;
+        }
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!jumpSound.isPlaying)
+                jumpSound.Play();
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+
+	void FixedUpdate () {
+        if (cannotMove)
+            horizontalMove = 0;
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+	}
+    
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }//
+}
